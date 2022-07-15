e291dac64dcfdd0556cdf1c34efd5a39d55b5d58
rjoyce <rjoyce@splunk.com>
2022-07-15 17:48:29 -0600

    Creates DynamoDb table as part of SAM app. Lambda writes entry to DynamoDb table.

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,33 +1,43 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
 let response;
+let databaseTableName = process.env.DatabaseTable;
+var AWS = require("aws-sdk");
+var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  var params = {
+    TableName: databaseTableName,
+    Item: {
+      ID: { S: "name" },
+    },
+  };
+  try {
+    // const ret = await axios(url);
+    data = await ddb.putItem(params).promise();
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "hello world",
+        // location: ret.data.trim()
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 
-    return response
+  return response;
 };
